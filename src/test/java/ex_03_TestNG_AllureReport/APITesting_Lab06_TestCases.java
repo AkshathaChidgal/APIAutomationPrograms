dc7f9e5d40e1ad5432918ee2ad62a6ba1a9c10bb
AkshathaChidgal <cakshathachidgal@gmail.com>
2025-05-10 19:38:49 +0530

    Lab06

--- src/test/java/ex_03_TestNG_AllureReport/APITesting_Lab06_TestCases.java
+++ src/test/java/ex_03_TestNG_AllureReport/APITesting_Lab06_TestCases.java
@@ -1,47 +1,18 @@
 package ex_03_TestNG_AllureReport;
 
-import groovy.transform.ASTTest;
 import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
 public class APITesting_Lab06_TestCases {
+    String pincode = "110048";
 
-
-    String pincode;
     @Test
-    public  void test_GET_POSITIVE_TC1()
-    {
-        String pincode ="574232";
+    public void test_GET_POSITIVE_TC1() {
+        pincode = "110048";
         RestAssured
                 .given()
                 .baseUri("https://api.zippopotam.us")
-                .basePath("/IN/"+pincode)
-                .when()
-                .get()
-                .then()
-                .log().all().statusCode(200);
-    }
-    @Test
-    public  void test_GET_NEGATIVE_TC2()
-    {
-        pincode ="@";
-        RestAssured
-                .given()
-                .baseUri("https://api.zippopotam.us")
-                .basePath("/IN/"+pincode)
-                .when()
-                .get()
-                .then()
-                .log().all().statusCode(200);
-    }
-    @Test
-    public  void test_GET_NEGATIVE_TC3()
-    {
-        pincode =" ";
-        RestAssured
-                .given()
-                .baseUri("https://api.zippopotam.us")
-                .basePath("/IN/"+pincode)
+                .basePath("/IN/" + pincode)
                 .when()
                 .get()
                 .then()
@@ -49,4 +20,30 @@ public class APITesting_Lab06_TestCases {
 
     }
 
+    @Test
+    public void test_GET_NEGATIVE_TC2() {
+        pincode = "@";
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/" + pincode)
+                .when()
+                .get()
+                .then()
+                .log().all().statusCode(200);
+    }
+
+    @Test
+    public void test_GET_NEGATIVE_TC3() {
+        pincode = " ";
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/USA/" + pincode)
+                .when()
+                .get()
+                .then()
+                .log().all().statusCode(200);
+
+    }
 }
